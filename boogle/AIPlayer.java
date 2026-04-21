e255a18f60c3fa8f5341181cf300a840fac36b5d
BlackFuffey <blackfuffey@invalid>
2026-04-21 10:54:23 -0400

    incomplete AIPlayer implementation

--- boogle/AIPlayer.java
+++ boogle/AIPlayer.java
@@ -1,9 +1,71 @@
 package boogle;
 
+import java.util.*;
+
 import boogle.Player;
 
+
 public class AIPlayer implements Player {
-    public AIPlayer() {
-        System.out.println("AIPlayer constructor called");
+
+    public enum Level{
+        PERFECT,    // always makes best move
+        SMART,      // randomly select from better half of possibilities
+        GOOD,       // randomly select from all possibilities
+        NORMAL,     // randomly select from worse half of possibilities
+        DUMB        // always makes worst move
     }
+
+    private Level level;
+
+    private FastOrderedSet<String> available;
+    private int worstCaseAt = 0;
+
+    private Random random = new Random();
+
+    public AIPlayer(Gameboard board, String[] wordlist, Level level) {
+        this.level = level;
+
+        // init logic
+    }
+
+    public String nextMove(String[] prevMoves) {
+        // update available move list
+        for (String move : prevMoves) {
+            available.remove(move);
+        }
+
+        // make a move according to the AI level
+        switch(this.level) {
+            case PERFECT: 
+                return available.pop();
+
+            case DUMB:
+                return available.shift();
+
+            case SMART:
+            case GOOD:
+            case NORMAL:
+                int half = available.size() / 2;
+                int target = random.nextInt(available.size() - half);
+
+                boolean useUpperHalf = 
+                    level!=Level.SMART ? 
+                        level!=Level.NORMAL ?
+                            random.nextBoolean() :
+                        false :
+                    true;
+                
+                int i = 0;
+                for (String move : useUpperHalf ? available : available.reverse()) {
+                    if (i == target) return move;
+                    i++;
+                }
+        }
+
+        // no move was successfully made, forfeit turn
+        return null;
+    }
+
+
+
 }
