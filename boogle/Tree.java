33a72754dbe1577b989bd75729b7954060a7eb66
BlackFuffey <blackfuffey@invalid>
2026-04-22 11:09:22 -0400

    add generic Tree data structure class

---
+++ boogle/Tree.java
@@ -0,0 +1,28 @@
+package boogle;
+
+import java.util.HashMap;
+
+public class Tree<T> {
+    private HashMap<T, Tree<T>> children = new HashMap<>();
+    private final T self;
+
+    public Tree(T self) {
+        this.self = self;
+    }
+
+    public T self() {
+        return self;
+    }
+
+    public void addChild(Tree<T> child) {
+        children.put(child.self, child);
+    }
+
+    public void removeChild(T value) {
+        children.remove(value);
+    }
+
+    public Tree<T> getChild(T value) {
+        return children.get(value);
+    }
+}
