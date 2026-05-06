0c5a00446461fbb19d0f084c11911c8dc6ff5485
SleepyEclipse <sleepyeclipse07@gmail.com>
2026-05-06 12:36:06 -0400

    Initial gui stuff ig (idk)

---
+++ src/boogle/ui/gui/GraphicalUI.java
@@ -0,0 +1,69 @@
+package boogle.ui.gui;
+import boogle.core.*;
+import boogle.core.Launcher.GameOptions;
+
+import javax.swing.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+class Windows extends JFrame{
+    public Windows(String title){
+        setTitle(title);
+    }
+    public void windowSize(boolean fullscreen){
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+    public void windowSize(int x, int y){
+        this.setSize(x,y);
+    }
+
+
+}
+
+public class GraphicalUI implements GameUI{
+
+    Windows mainMenu = new Windows("Main");
+    public boolean lobby(GameOptions options) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'lobby'");
+    }
+    public void newTurn(Gameboard board, HashMap<Player, Integer> scoreboard, ArrayList<String> playedWords,
+            Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'newTurn'");
+    }
+    public void passive() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'passive'");
+    }
+    public String active() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'active'");
+    }
+    public void confirm() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'confirm'");
+    }
+    public void results(List<Entry<String, Integer>> leaderboard, int skips, int maxScore) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'results'");
+    }
+    public void close() throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'close'");
+    }
+    @Override
+    public void startTurn(Gameboard board, List<Entry<String, Integer>> leaderboard, ArrayList<String> playedWords,
+            String currentPlayerName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startTurn'");
+    }
+    public void endTurn(TurnStatus status, String move, int scoreGained, int minWordLength) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'endTurn'");
+    }
+    
+}
